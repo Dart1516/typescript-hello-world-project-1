6bd8115e2976e111acdf96b5bc9591f21c31344d
Dart1516 <david_daniel_puche_ferrer@hotmail.com>
2026-09-04 02:27:49 -0300

    this is my first assigment

---
+++ README.md
@@ -0,0 +1,27 @@
+# Overview
+
+With this project I'm traying to learn about how to program in typescript. This will open the door for new technologies. 
+
+I will create a command-line vocabulary learning tool for English. It works like a dictionary, but instead of showing only the definition of a word, it also expands two related words found inside that definition to give better context. 
+
+I chose this idea because people like me who are learning English often look up a word and still cannot fully understand its definition, since the definition itself contains unfamiliar words.
+ 
+
+
+This allows us to create larger and safer projects, which would otherwise be a nightmare to maintain.
+
+{Provide a link to your YouTube demonstration.  It should be a one minute demo of the software running and a walkthrough of the code.}
+
+[Software Demo Video](https://youtu.be/E4KCFVQHX1M)
+
+# Development Environment
+
+{Describe the tools that you used to develop the software}
+
+I’m using TypeScript. According to what I’ve learned, TypeScript is a superset of JavaScript. It is basically JavaScript, but with additional features that help identify and prevent errors that are commonly found in JavaScript.
+
+# Useful Websites
+
+{Make a list of websites that you found helpful in this project}
+* [TypeScript] https://www.typescriptlang.org/docs/handbook/typescript-in-5-minutes.html
+* [Web Site Name](http://url.link.goes.here)
---
+++ practicing.ts
@@ -0,0 +1,66 @@
+const user1 = {
+    name: "David",
+    id: 1,
+}
+console.log(typeof user1.name);
+console.log(typeof user1.id);   
+
+interface User {
+    name: string;
+    id: number;
+}
+
+ 
+class UserAccount {
+    name: string;
+    id: number;
+    constructor(name: string, id: number) {
+        this.name = name;
+        this.id = id;
+    }
+}
+
+const user: User = new UserAccount("David", 1); 
+console.log(user.name); 
+console.log(user.id); 
+
+
+/* Composing types*/ 
+
+type Myboolean = true | false;
+const myBool: Myboolean = true;
+console.log(myBool);
+ 
+let s: string;
+s = "Hello, World!";
+console.log(typeof s);
+let n: number = 42;
+console.log(typeof n);
+
+function envolverEnArreglo(valor: string | string[]): string[] {
+  if (typeof valor === "string") {
+    return [valor];   // aquí TypeScript sabe que `valor` es string
+  }
+  return valor;       // aquí sabe que ya es string[] porque en la linea anterior lo devolvió como arreglo
+}
+
+    /*generic  
+
+    means a value that can be anything */
+    type StringArray = Array<string>;
+type NumberArray = Array<number>;
+type ObjectWithNameArray = Array<{ name: string }>;
+
+/*we can declare your own types that use generics*/ 
+
+interface Backpack<Type> {
+  add: (obj: Type) => void;
+  get: () => Type;
+}
+ 
+// This line is a shortcut to tell TypeScript there is a
+// constant called `backpack`, and to not worry about where it came from.
+declare const backpack: Backpack<string>;
+ 
+// object is a string, because we declared it above as the variable part of Backpack.
+const object = backpack.get();
